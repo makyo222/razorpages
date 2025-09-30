1bbbb0597ed5e4020c10e936f5f116676e9285d0
makyo222 <markobaby@gmail.com>
2025-09-30 09:30:18 +0200

    DbContext és SQLite adatbázis kapcsolat beállítása

---
+++ Migrations/20250930072829_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication2.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Barlangok",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    nev = table.Column<string>(type: "TEXT", nullable: false),
+                    hossz = table.Column<int>(type: "INTEGER", nullable: false),
+                    kiterjedes = table.Column<int>(type: "INTEGER", nullable: false),
+                    melyseg = table.Column<int>(type: "INTEGER", nullable: false),
+                    magassag = table.Column<int>(type: "INTEGER", nullable: false),
+                    telepules = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Barlangok", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Barlangok");
+        }
+    }
+}
