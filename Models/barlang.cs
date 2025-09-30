271fb7a1c7b05941495ee25fe1427181c87c260f
makyo222 <markobaby@gmail.com>
2025-09-30 10:05:27 +0200

    szűrés

--- Models/barlang.cs
+++ Models/barlang.cs
@@ -3,11 +3,11 @@
     public class barlang
     {
         public int id { get; set; }
-        public string nev { get; set; }
+        public string nev { get; set; } = string.Empty;
         public int hossz { get; set; }
         public int kiterjedes { get; set; }
         public int melyseg { get; set; }
         public int magassag { get; set; }
-        public string telepules { get; set; }
+        public string telepules { get; set; } = string.Empty;
     }
 }
---
+++ Pages/Barlangok/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+using WebApplication2.Models;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Pages.Barlangok
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BarlangDbContext _context;
+
+        public IndexModel(BarlangDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<barlang> BarlangList { get; set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchTelepules { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var query = _context.Barlangok.AsQueryable();
+
+            if (!string.IsNullOrEmpty(SearchTelepules))
+            {
+                query = query.Where(b => b.telepules.Contains(SearchTelepules));
+            }
+
+            BarlangList = await query.ToListAsync();
+        }
+    }
+}
+
